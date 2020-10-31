9c3777d30db26f9925777603e799da130a79b24e
avpimblesr <allan.pimble@gmail.com>
2020-10-31 18:05:26 -0400

    first commit

---
+++ src/components/todos-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TodosList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Todos List Component</p>
+      </div>
+    )
+  }
+}
